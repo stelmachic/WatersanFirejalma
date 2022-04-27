e71276459bc7e680a85ead2f87a4a7ac0fa3e966
Allan Kley <allanlkley@gmail.com>
2022-04-27 09:29:56 -0300

    colocando animacao e idle

--- Watersan_e_Firejalma/Watersan_e_Firejalma/Form1.cs
+++ Watersan_e_Firejalma/Watersan_e_Firejalma/Form1.cs
@@ -18,6 +18,7 @@ namespace Watersan_e_Firejalma
         Timer tm = new Timer();
         Timer anim = new Timer();
         Timer idle = new Timer();
+  
 
         Image img = new Bitmap(@"C:\Users\Aluno\Documents\a\Arte\Sprite Sheet trevisharp.png");
         int x = 20;
@@ -25,49 +26,60 @@ namespace Watersan_e_Firejalma
         int dg = 1;
         int dx = 0;
         int dy = 0;
-
+        int resolucaox = 32;
+        int resolucaoy = 32;
 
         public Form1()
         {
             InitializeComponent();
             tm.Interval = 25;
             anim.Interval = 50;
-            idle.Interval = 50;
+            idle.Interval = 25;
 
             int sx = 0;
 
+
             tm.Tick += delegate
             {
-
                 x += dx;
                 dy += dg;
                 y += dy;
 
-                if (y + 200 > pb.Height)
+                if (y + 200 > bmp.Height) // chão
                 {
                     dy = 0;
-                    y = pb.Height - 200;
+                    y = bmp.Height - 200;
                 }
+                if (x + 200 > bmp.Width) // parede direita
+                {
+                    dx = 0;
+                    x = bmp.Width - 200;
+                }
+                if (x < 0) // parede esquerda
+                {
+                    dx = 0;
+                    x = 0;
+                }
+
+                pb.Image = bmp;
             };
 
             anim.Tick += delegate
             {
                 g.Clear(Color.White);
+
                 sx += 32;
                 if (sx == 32 * 8)
                     sx = 0;
 
-                g.DrawImage(img, new Rectangle(x, y, 200, 200), new Rectangle(sx, 0, 32, 32), GraphicsUnit.Pixel);
-                pb.Image = bmp;
-
-
+                g.DrawImage(img, new Rectangle(x, y, 200, 200), new Rectangle(sx, 0, resolucaox, resolucaoy), GraphicsUnit.Pixel);
             };
 
             idle.Tick += delegate
             {
                 g.Clear(Color.White);
-                g.DrawImage(img, new Rectangle(x, y, 200, 200), new Rectangle(0, 0, 32, 32), GraphicsUnit.Pixel);
-                pb.Image = bmp;
+
+                g.DrawImage(img, new Rectangle(x, y, 200, 200), new Rectangle(0, 0, resolucaox, resolucaoy), GraphicsUnit.Pixel);
             };
         }
 
@@ -81,18 +93,19 @@ namespace Watersan_e_Firejalma
                     Application.Exit();
                     break;
                 case Keys.Left:
-                    anim.Start();
                     idle.Stop();
+                    anim.Start();
                     dx = -15;
                     break;
                 case Keys.Right:
-                    anim.Start();
                     idle.Stop();
+                    anim.Start();
                     dx = 15;
                     break;
                 case Keys.Up:
                     if (y == bmp.Height - 200)
                     {
+                        idle.Start();
                         dy = -20;
                     }
                     break;
@@ -101,8 +114,10 @@ namespace Watersan_e_Firejalma
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            bmp = new Bitmap(pb.Width, pb.Height);
+            g = Graphics.FromImage(bmp);
             tm.Start();
+            idle.Start();
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
